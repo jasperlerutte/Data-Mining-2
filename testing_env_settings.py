d62b7b3ec6a6a496f904fa0b6be03ec889f65a60
robbert-rog <robbertrog@gmail.com>
2024-05-08 12:51:49 +0200

    basics

---
+++ testing_env_settings.py
@@ -0,0 +1,12 @@
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values
+
+
+# loading variables from .env file
+load_dotenv()
+
+# accessing and printing value
+path = os.getenv("data_path")
+print(os.getenv("data_path"))
